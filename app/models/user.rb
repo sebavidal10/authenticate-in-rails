8077c8d6a9868220c3ad5c7c50f63268bfded4df
Sebastián Vidal <sebastian@intercode.cl>
2020-11-17 21:58:18 -0300

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, :omniauth_providers => [:linkedin, :google_oauth2, :github, :facebook]
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,10]
+    end
+  end
+         
+end
